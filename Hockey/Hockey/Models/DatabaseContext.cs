95926cba385404e8a04b3040fcdbaa619006c84c
emilrylander <45594702+emilrylander@users.noreply.github.com>
2019-10-16 14:48:33 +0200

    Added files + models for the tables (and some other stuff)

---
+++ Hockey/Hockey/Models/DatabaseContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using MySql.Data.Entity;
+
+namespace Hockey.Models
+{
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base("DefaultConnection")
+        {
+
+        }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Arena> Arenas { get; set; }
+        public DbSet<Match> Matches { get; set; }
+    }
+}
---
+++ Hockey/Hockey/Models/HockeyDbTables.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hockey.Models
+{
+
+    public class Team
+    {
+        public int ID { get; set; }
+        public string Teamname { get; set; }
+    }
+    public class Arena
+    {
+        public int ID { get; set; }
+        public string Arenaname { get; set; }
+    }
+    public class Match
+    {
+        public int ID { get; set; }
+        public int ArenaID { get; set; }
+        public int HomeTeamID { get; set; }
+        public int HomeTeamScore { get; set; }
+        public int GoneTeamID { get; set; }
+        public int GoneTeamScore { get; set; }
+
+    }
+}
